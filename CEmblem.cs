520c9f14ec9e4ec18e29a5cbc92efcd0214b88a9
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2021-04-02 18:34:17 +0300

    Add project files.

---
+++ CEmblem.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emblem
+{
+    class CEmblem
+    {   Color color { get; set; }
+        public Color currentColor { get; set; }
+        string name { get; set; }
+        int Size { get; set; }
+        int X { get; set; }
+        int Y { get; set; }
+        Pen p = new Pen(Color.Red,1f);
+        public CEmblem() { }
+        public CEmblem(int x, int y, int size, string name, Color color)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Size = size;
+            this.name = name;
+            this.color = color;
+            currentColor = color;
+        }
+        public CEmblem(int x, int y, int size, Color color)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Size = size;
+            this.color = color;
+            currentColor = color;
+        }
+        public void Hide()
+        {
+            currentColor = Color.White;
+        }
+        public void Show()
+        {
+            currentColor = color;
+        }
+        public void MoveRight()
+        {
+            X++;
+        }
+        public void MoveLeft()
+        {
+            X--;
+        }
+        public void MoveUp()
+        {
+            Y--;
+        }
+        public void MoveDown()
+        {
+            Y++;
+        }
+        public void IncreaseSize()
+        {
+            Size++;
+        }
+        public void ReduceSize()
+        {
+            Size--;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+        public void Draw(PictureBox pictureBox)
+        {
+            p.Color = currentColor;
+            using (var g = Graphics.FromImage(pictureBox.Image))
+            {
+                g.DrawEllipse(p, X, Y, Size, Size);
+                double d = Size;
+                float side = (float)(d * (Math.Sqrt(2f) / 2f));
+                g.DrawRectangle(p, X+ Size/7, Y+ Size/7, side, side);
+                Rectangle r = new Rectangle(X + Size / 7, Y + Size / 7, (int)side, (int)side);
+
+                RotateRectangle(g, r, 45, p);
+       
+                pictureBox.Refresh();
+            }
+            
+        }
+        public void RotateRectangle(Graphics g, Rectangle r, float angle, Pen p)
+        {
+            using (Matrix m = new Matrix())
+            {
+                m.RotateAt(angle, new PointF(r.Left + (r.Width / 2),
+                                          r.Top + (r.Height / 2)));
+                g.Transform = m;
+                g.DrawRectangle(p, r);
+                g.ResetTransform();
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emblem
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+        }
+        int count = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            int rand = r.Next(5, 434);
+            int rand2 = r.Next(5, 434);
+            int rand3 = r.Next(5, 234);
+            CEmblem emblem = new CEmblem(rand, rand2, rand3, "Emblem" + count, Color.Black);
+            emblem.Show();
+            emblem.Draw(pictureBox2);
+            comboBox1.Items.Add(emblem);  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null) {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.MoveUp();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.MoveLeft();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.MoveRight();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.MoveDown();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.ReduceSize();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                if (emblem.currentColor == Color.White) { return; }
+                emblem.Hide();
+                emblem.Draw(pictureBox2);
+                emblem.IncreaseSize();
+                emblem.Show();
+                emblem.Draw(pictureBox2);
+            }
+        }
+    }
+}
